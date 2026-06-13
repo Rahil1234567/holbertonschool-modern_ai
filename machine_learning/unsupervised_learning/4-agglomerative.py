81211ef9d22c4239fca671adc9bbcacc988ada97
Rahil1234567 <129419526+Rahil1234567@users.noreply.github.com>
2026-06-14 01:07:59 +0400

    Create 4-agglomerative.py

---
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+This module contains the function Agglomerative_Clustering.
+"""
+from sklearn import cluster, metrics
+Apply_PCA = __import__('1-pca').Apply_PCA
+
+
+def Agglomerative_Clustering(X, n_clusters, random_state=None,
+                             n_components=None, use_pca_data=True):
+    """
+    Performs Agglomerative hierarchical clustering.
+
+    Args:
+        X (numpy.ndarray): Tabular data.
+        n_clusters (int): Number of clusters.
+        random_state (int): Seed for reproducibility.
+        n_components (int): PCA components to reduce to.
+        use_pca_data (bool): Whether to apply PCA.
+
+    Returns:
+        tuple: (fitted_model, X_data, silhouette_score)
+    """
+    # 1. Dimensionality Reduction
+    if use_pca_data:
+        X_used, _ = Apply_PCA(X, n_components=n_components,
+                              random_state=random_state)
+    else:
+        X_used = X
+
+    # 2. Clustering
+    model = cluster.AgglomerativeClustering(n_clusters=n_clusters)
+    model.fit(X_used)
+
+    # 3. Evaluation
+    score = None
+    if n_clusters > 1:
+        score = metrics.silhouette_score(X_used, model.labels_)
+
+    return model, X_used, score
